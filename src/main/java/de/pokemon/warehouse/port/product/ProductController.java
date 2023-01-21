5606faaad1b21aa18bf63818437587f540ba40a7
Tom Magnus Petersen <vedrfoelnir@toms-mbp-2.local>
2023-01-21 12:55:33 +0100

    initial commit; move from private repo

--- src/main/java/de/pokemon/warehouse/port/product/ProductController.java
+++ src/main/java/de/pokemon/warehouse/port/product/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = {"https://localhost:3000/"})
 public class ProductController {
 
     @Autowired
--- src/test/java/de/pokemon/warehouse/ProductControllerITests.java
+++ src/test/java/de/pokemon/warehouse/ProductControllerITests.java
@@ -1,13 +1,7 @@
 package de.pokemon.warehouse;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import de.pokemon.warehouse.core.domain.service.interfaces.ProductRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
