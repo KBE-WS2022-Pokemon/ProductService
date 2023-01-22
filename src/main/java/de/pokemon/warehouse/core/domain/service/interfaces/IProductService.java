90f217b39b5c91ee822673b030a6025e02692745
Tom Magnus Petersen <vedrfoelnir@toms-mbp-2.local>
2023-01-22 16:33:47 +0100

    implemented uuid usage with database

--- src/main/java/de/pokemon/warehouse/core/domain/service/interfaces/IProductService.java
+++ src/main/java/de/pokemon/warehouse/core/domain/service/interfaces/IProductService.java
@@ -4,13 +4,15 @@ import de.pokemon.warehouse.core.domain.model.Product;
 import de.pokemon.warehouse.port.product.exception.ProductNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public interface IProductService {
     Iterable<Product> getAllProducts();
 
-    Product getProduct(Long id) throws ProductNotFoundException;
+    Product getProduct(UUID id) throws ProductNotFoundException;
 
     Product save(Product product);
 
-    void deleteProduct(Long id);
+    void deleteProduct(UUID id);
 }
