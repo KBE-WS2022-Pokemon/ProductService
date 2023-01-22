4b0b808e5e96f7f77ed9dd3ba9247d8a5ffc0198
Tom Magnus Petersen <vedrfoelnir@toms-mbp-2.local>
2023-01-22 14:36:37 +0100

    rabbit-mq done?

--- src/main/java/de/pokemon/warehouse/core/domain/service/impl/ProductConverter.java
+++ src/main/java/de/pokemon/warehouse/core/domain/service/impl/ProductConverter.java
@@ -3,7 +3,9 @@ package de.pokemon.warehouse.core.domain.service.impl;
 import de.pokemon.warehouse.core.domain.model.CartProductDto;
 import de.pokemon.warehouse.core.domain.model.Product;
 import de.pokemon.warehouse.core.domain.service.interfaces.IProductConverter;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProductConverter implements IProductConverter {
     @Override
     public CartProductDto convert(Product product) {
--- src/main/java/de/pokemon/warehouse/core/domain/service/interfaces/IRabbitMQService.java
+++ src/main/java/de/pokemon/warehouse/core/domain/service/interfaces/IRabbitMQService.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Service;
 @Service
 public interface IRabbitMQService {
 
-
     void sendProductToCart(CartProductDto dto);
 }
